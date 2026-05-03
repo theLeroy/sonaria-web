eee6ac8e5d77c2c3387fea3b0b839fbab765ece6
theLeroy <janis.wanger@novu.ch>
2026-05-03 12:58:02 +0200

    Create ticket.vue

---
+++ app/pages/ticket.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Tickets',
+})
+
+// on opeing of the page, check if the url contains a token direct to eventfrog
+const link = 'https://eventfrog.ch/de/p/festivals/electronic/sonaria-festival-7441844412533991522.html'
+
+onMounted(() => {
+  // redirect to eventfrog
+  window.location.href = link
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>Tickets</h1>
+  </div>
+</template>
